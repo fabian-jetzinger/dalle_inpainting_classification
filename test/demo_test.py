6d6dcd97d4eaa26c39c190297d0320166332823d
Fabian Jetzinger <S2110595008@fhooe.at>
2023-01-25 20:21:34 +0100

    try failed test

--- test/demo_test.py
+++ test/demo_test.py
@@ -12,7 +12,7 @@ def factorial(n):
     """Return the factorial of n, an exact integer >= 0.
 
     >>> [factorial(n) for n in range(6)]
-    [1, 1, 2, 6, 24, 120]
+    [1, 1, 2, 6, 24, 121]
     >>> factorial(30)
     265252859812191058636308480000000
     >>> factorial(-1)
